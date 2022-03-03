7a250e1b9d25e87f28ecc7a1e5944fec731d5f50
monthalcantara <montival_junior@yahoo.com.br>
2022-03-03 19:20:34 -0300

    Adicionado projeto de estudo sobre Hateoas

---
+++ Api-Estudo-SpringHateoas/src/main/java/io/github/monthalcantara/EstudoSpringHateoas/EstudoSpringHateoasApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.EstudoSpringHateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpringHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpringHateoasApplication.class, args);
+	}
+
+}
